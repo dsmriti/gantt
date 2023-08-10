3fda3fd69d9aae557d05b463694fed3883bcb720
dsmriti <iamsmritid@gmail.com>
2023-08-11 00:10:55 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ GanttModule(Highcharts);
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   Highcharts: typeof Highcharts = Highcharts; // required
@@ -16,27 +16,27 @@ export class AppComponent implements OnInit {
     chart: {
       scrollablePlotArea: {
         minWidth: 40 * 48, // 48 weeks, 40px each
-        scrollPositionX: 0 // Start at beginning
+        scrollPositionX: 0, // Start at beginning
       },
-      height: '275'
+      height: '275',
     },
     credits: {
-      enabled: false
+      enabled: false,
     },
     plotOptions: {
       gantt: {
-        pointWidth: 50
+        pointWidth: 50,
       },
       series: {
         states: {
           hover: {
-            enabled: true // Enable hover state
+            enabled: true, // Enable hover state
           },
           inactive: {
-            opacity: 1 // Ensures that all data points remain fully visible even if one is hovered over
-          }
-        }
-      }
+            opacity: 1, // Ensures that all data points remain fully visible even if one is hovered over
+          },
+        },
+      },
     },
     /** MONTH WISE x-Axis*/
     // xAxis: [{
@@ -85,13 +85,13 @@ export class AppComponent implements OnInit {
           format: '{value:%W}',
           style: {
             color: '#454343', // Text color of the label
-            backgroundColor: '#FBFBFB' // Background color of the label
-          }
+            backgroundColor: '#FBFBFB', // Background color of the label
+          },
         },
         grid: {
           enabled: true,
           borderColor: '#e2e5eb',
-          cellHeight: 32
+          cellHeight: 32,
         },
         lineColor: '#FBFBFB',
         tickInterval: 7 * 24 * 3600 * 1000,
@@ -109,31 +109,31 @@ export class AppComponent implements OnInit {
               text: 'Stock Limit',
               rotation: 270,
               y: 15,
-              textAlign: 'right'
-            }
-          }
-        ]
-      }
+              textAlign: 'right',
+            },
+          },
+        ],
+      },
     ],
     yAxis: {
       categories: ['Category 1', 'Category 2', 'Category 3'],
       grid: {
-        enabled: false
+        enabled: false,
       },
       labels: {
-        enabled: false // This will hide the y-axis labels
+        enabled: false, // This will hide the y-axis labels
       },
       title: {
-        text: null // This will hide the y-axis title
+        text: null, // This will hide the y-axis title
       },
       plotLines: [
         {
-          width: 20
-        }
-      ]
+          width: 20,
+        },
+      ],
     },
     tooltip: {
-      enabled: true
+      enabled: true,
     },
     series: [
       {
@@ -144,9 +144,9 @@ export class AppComponent implements OnInit {
             start: Date.UTC(2023, 1, 2),
             end: Date.UTC(2023, 5, 22),
             color: '#f4f5f7',
-            y: 0
-          }
-        ]
+            y: 0,
+          },
+        ],
       },
       {
         name: 'Category 2',
@@ -156,9 +156,9 @@ export class AppComponent implements OnInit {
             start: Date.UTC(2023, 6, 2),
             end: Date.UTC(2023, 8, 22),
             color: '#f4f5f7',
-            y: 1
-          }
-        ]
+            y: 1,
+          },
+        ],
       },
       {
         name: 'Category 3',
@@ -168,115 +168,123 @@ export class AppComponent implements OnInit {
             start: Date.UTC(2023, 8, 1),
             end: Date.UTC(2023, 11, 22),
             color: '#f4f5f7',
-            y: 2
-          }
-        ]
+            y: 2,
+          },
+        ],
       },
       {
         name: 'Assignee 1',
         type: 'scatter',
         marker: {
-          symbol: 'url(./assets/testing.png)'
+          symbol:
+            'url(https://github.com/dsmriti/gantt/blob/master/src/assets/testing.png)',
         },
         data: [
           {
             x: Date.UTC(2023, 2, 6),
-            y: 0
-          }
-        ]
+            y: 0,
+          },
+        ],
       },
       {
         name: 'Assignee 2',
         type: 'scatter',
         marker: {
-          symbol: 'url(./assets/search.png)'
+          symbol:
+            'url(https://github.com/dsmriti/gantt/blob/master/src/assets/search.png)',
         },
         data: [
           {
             x: Date.UTC(2023, 2, 9),
-            y: 0
-          }
-        ]
+            y: 0,
+          },
+        ],
       },
       {
         name: 'Assignee 3',
         type: 'scatter',
         marker: {
-          symbol: 'url(./assets/truck.png)'
+          symbol:
+            'url(https://github.com/dsmriti/gantt/blob/master/src/assets/truck.png)',
         },
         data: [
           {
             x: Date.UTC(2023, 4, 7),
-            y: 0
-          }
-        ]
+            y: 0,
+          },
+        ],
       },
       {
         name: 'Assignee 4',
         type: 'scatter',
         marker: {
-          symbol: 'url(./assets/warehouse.png)'
+          symbol:
+            'url(https://github.com/dsmriti/gantt/blob/master/src/assets/warehouse.png)',
         },
         data: [
           {
             x: Date.UTC(2023, 5, 7),
-            y: 0
-          }
-        ]
+            y: 0,
+          },
+        ],
       },
       {
         name: 'Assignee 5',
         type: 'scatter',
         marker: {
-          symbol: 'url(./assets/testing.png)'
+          symbol:
+            'url(https://github.com/dsmriti/gantt/blob/master/src/assets/testing.png)',
         },
         data: [
           {
             x: Date.UTC(2023, 8, 14),
-            y: 1
-          }
-        ]
+            y: 1,
+          },
+        ],
       },
       {
         name: 'Assignee 6',
         type: 'scatter',
         marker: {
-          symbol: 'url(./assets/search.png)'
+          symbol:
+            'url(https://github.com/dsmriti/gantt/blob/master/src/assets/search.png)',
         },
         data: [
           {
             x: Date.UTC(2023, 8, 17),
-            y: 2
-          }
-        ]
+            y: 2,
+          },
+        ],
       },
       {
         name: 'Assignee 7',
         type: 'scatter',
         marker: {
-          symbol: 'url(./assets/truck.png)'
+          symbol:
+            'url(https://github.com/dsmriti/gantt/blob/master/src/assets/truck.png)',
         },
         data: [
           {
             x: Date.UTC(2023, 9, 17),
-            y: 2
-          }
-        ]
+            y: 2,
+          },
+        ],
       },
       {
         name: 'Assignee 8',
         type: 'scatter',
         marker: {
-          symbol: 'url(./assets/warehouse.png)'
+          symbol:
+            'url(https://github.com/dsmriti/gantt/blob/master/src/assets/warehouse.png)',
         },
         data: [
           {
             x: Date.UTC(2023, 10, 16),
-            y: 2
-          }
-        ]
-      }
-    ]
+            y: 2,
+          },
+        ],
+      },
+    ],
   };
 
   ngOnInit(): void {}
